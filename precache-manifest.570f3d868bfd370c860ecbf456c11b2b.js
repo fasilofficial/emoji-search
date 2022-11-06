3dc3abd0d06bff240ed64b97cac7faaeb7c8be19
fasilofficial <muhammedfasilofficial@gmail.com>
2022-11-06 17:25:32 +0530

    Updates

---
+++ precache-manifest.570f3d868bfd370c860ecbf456c11b2b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34d19a9d3a9e7efabeb62a0266eb3d10",
+    "url": "/emoji-search/index.html"
+  },
+  {
+    "revision": "47cde07b2d6f77ce9b10",
+    "url": "/emoji-search/static/css/2.90da6880.chunk.css"
+  },
+  {
+    "revision": "731a522cfb9be9ffda7d",
+    "url": "/emoji-search/static/css/main.65605343.chunk.css"
+  },
+  {
+    "revision": "47cde07b2d6f77ce9b10",
+    "url": "/emoji-search/static/js/2.b81e05a8.chunk.js"
+  },
+  {
+    "revision": "6120b983a03909b7d2cd9bf134169811",
+    "url": "/emoji-search/static/js/2.b81e05a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "731a522cfb9be9ffda7d",
+    "url": "/emoji-search/static/js/main.29762620.chunk.js"
+  },
+  {
+    "revision": "53f65d824d8bd704bdf5",
+    "url": "/emoji-search/static/js/runtime-main.fcefcbdb.js"
+  }
+]);
